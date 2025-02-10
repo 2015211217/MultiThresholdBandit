a2542ade80cf185602fdc47646a125e611c0581a
JXK <jiangxuanke@gmail.com>
2025-02-11 03:44:48 +0900

    first step

---
+++ TrivialSolver.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def TrivialSolver(arms = int, objectives = int, inputMatrix):
+    
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import numpy as np
-
+from TrivialSolver import TrivialSolver
 M = 4
 K = 10
 bmmu_1 = [0.1, 0.1, 0.1, 0.35, 0.45, 0.55, 0.65, 0.2, 0.2, 0.2]
-bmmu_2 = [0.2, 0.4 - np.power(0.2, 2), 0.4 - np.power(0.2, 3), 0.4 - np.power(0.2, 4), 0.45, 0.55, 0.6 + np.power(0.1, 5), 0.6 + np.power(0.1, 4), 0.6 + np.power(0.1, 3), 0.6 + np.power(0.1, 2), 0.6 + np.power(0.1, 1)]
+bmmu_2 = [0.2, round(0.4 - np.power(0.2, 2), 6), round(0.4 - np.power(0.2, 3), 6),round(0.4 - np.power(0.2, 4), 6)
+    , 0.45, 0.55, round(0.6 + np.power(0.1, 5), 6), round(0.6 + np.power(0.1, 4), 6), 0.6 + round(np.power(0.1, 3), 6), round(0.6 + np.power(0.1, 2), 6), round(0.6 + np.power(0.1, 1), 6)]
 bmmu_3 = [0.05, 0.1, 0.15, 0.2, 0.45, 0.55, 0.65, 0.7, 0.75, 0.8]
 bmmu_4 = [0.4, 0.4, 0.4, 0.4, 0.5, 0.5, 0.5, 0.5, 0.6, 0.6]
-simga = np.random.rand()
+sigma = round(np.random.rand(), 6)
 
 ###form the input matrix
 input_matrix = [bmmu_1, bmmu_2, bmmu_3, bmmu_4]
 print(input_matrix)
-
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    TrivialSolver(K, M, input_matrix)
