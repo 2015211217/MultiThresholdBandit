5a0440f8c3d77797931f8847173a75a131d7c27d
JXK <jiangxuanke@gmail.com>
2025-02-13 03:12:32 +0900

    2.13

--- main.py
+++ main.py
@@ -6,25 +6,41 @@ from MultiTUCB import MultiTUCB
 
 M = 4
 K = 10
+repetation = 1000
 bmmu_1 = [0.1, 0.1, 0.1, 0.35, 0.45, 0.55, 0.65, 0.2, 0.2, 0.2]
 bmmu_2 = [0.2, round(0.4 - np.power(0.2, 2), 6), round(0.4 - np.power(0.2, 3), 6),round(0.4 - np.power(0.2, 4), 6)
     , 0.45, 0.55, round(0.6 + np.power(0.1, 5), 6), round(0.6 + np.power(0.1, 4), 6), 0.6 + round(np.power(0.1, 3), 6), round(0.6 + np.power(0.1, 2), 6), round(0.6 + np.power(0.1, 1), 6)]
 bmmu_3 = [0.05, 0.1, 0.15, 0.2, 0.45, 0.55, 0.65, 0.7, 0.75, 0.8]
 bmmu_4 = [0.4, 0.4, 0.4, 0.4, 0.5, 0.5, 0.5, 0.5, 0.6, 0.6]
-##过后调整这个标准
-thresholds = [0.4, 0.4, 0.4, 0.4]
 
+thresholds = [0.5, 0.7, 0.6, 0.5]
+plotpoint = 10
 sigma = np.random.rand()
 epsilon = 0.005
 delta = 0.005
 # hatmu[i][m] = feedbackMatrix[t][i][m]
-T0 = 1000
-feedbackMatrix = np.random.random(size = [T0, K, M])
-###form the input matrix
-input_matrix = [bmmu_1, bmmu_2, bmmu_3, bmmu_4]
-# print(input_matrix)
-if __name__ == '__main__':
-    resultTrivialSolver = TrivialSolver(K, M, T0, sigma, epsilon, delta, feedbackMatrix, thresholds)
-    resultAPTG = MultiAPTG(K, M, T0, sigma, epsilon, delta, feedbackMatrix, thresholds)
-    resultHDoC = MultiHDoC(K, M, T0, sigma, epsilon, delta, feedbackMatrix, thresholds)
-    resultOurs = MultiTUCB(K, M, T0, sigma, epsilon, delta, feedbackMatrix, thresholds)
+T0 = 100000
+# 10 个数据点
+resultTrivialSolver = np.zeros((repetation, plotpoint))
+resultAPTG = np.zeros((repetation, plotpoint))
+resultHDoC = np.zeros((repetation, plotpoint))
+resultOurs = np.zeros((repetation, plotpoint))
+
+#每个算法十个点， 横轴是delta，纵轴是
+for DeltaMultipler in range(plotpoint):
+    for round in range(repetation):
+        feedbackMatrix = np.random.random(size = [T0, K, M])
+        ###form the input matrix
+        delta *= (DeltaMultipler + 1)
+        input_matrix = [bmmu_1, bmmu_2, bmmu_3, bmmu_4]
+    # print(input_matrix)
+        if __name__ == '__main__':
+            resultTrivialSolver[round][repetation] = TrivialSolver(K, M, T0, sigma, epsilon, delta, feedbackMatrix, thresholds)
+            resultAPTG[round][repetation] = MultiAPTG(K, M, T0, sigma, epsilon, delta, feedbackMatrix, thresholds)
+            resultHDoC[round][repetation] = MultiHDoC(K, M, T0, sigma, epsilon, delta, feedbackMatrix, thresholds)
+            resultOurs[round][repetation] = MultiTUCB(K, M, T0, sigma, epsilon, delta, feedbackMatrix, thresholds)
+
+#calculate the deviations
+#check the presentation of deviation, should the upper bound and lower bound be the same number?
+deviationTrivialSolver = np.zeros(plotpoint)
+deviationAPTG = np.zeros(plotpoint)
--- zPlot.py
+++ zPlot.py
@@ -11,10 +11,10 @@ timearray = np.zeros(plot_x_number)
 for i in range(plot_x_number):
     timearray[i] = (i+1) * devices_block
 
-plt.plot(timearray,[12.621,30.809,45.402, 60.939, 74.219, 89.928, 105.938, 123.536, 141.933, 159.463],"k-.d",label="Random")
-plt.plot(timearray,[19.57, 43.75, 81.17, 121.22, 161.35, 199.45, 239.11, 276.87, 314.57, 353.18],"g-.^",label="EH-Hungarian[11]")
-plt.plot(timearray,[55.437, 94.035, 133.201, 172.487, 211.143, 249.755, 288.074, 326.65, 363.16, 398.392],color = "tab:blue", linestyle = '-.', marker = '*', label="Bi-Hungarian[16]")
-plt.plot(timearray,[115.577, 180.983, 228.775, 268.296, 302.765, 333.714, 362.136, 388.448, 413.145, 436.473],"r-.h",label="EHTO-SM")
+plt.plot(timearray,[12.621,30.809,45.402, 60.939, 74.219, 89.928, 105.938, 123.536, 141.933, 159.463],"k-.d",label="Trivial Solver")
+plt.plot(timearray,[19.57, 43.75, 81.17, 121.22, 161.35, 199.45, 239.11, 276.87, 314.57, 353.18],"g-.^",label="MultiAPT-G")
+plt.plot(timearray,[55.437, 94.035, 133.201, 172.487, 211.143, 249.755, 288.074, 326.65, 363.16, 398.392],color = "", linestyle = '-.', marker = '*', label="MultiHDoC")
+plt.plot(timearray,[115.577, 180.983, 228.775, 268.296, 302.765, 333.714, 362.136, 388.448, 413.145, 436.473],"r-.h",label="MultiTUCB")
 
 # plt.yticks(np.arange(1, 6, step = 4))
 plt.tick_params(labelsize = 14)
@@ -27,9 +27,9 @@ plt.xlim(xmin = 1)
 
 plt.grid(b=None, which='major', axis='both')
 # plt.title("")
-plt.xlabel("Length of Time Slot (ms)", fontsize = 14, fontweight = 'bold')
+plt.xlabel("", fontsize = 14, fontweight = 'bold')
 
-plt.ylabel("Average No. of Connected Devices", fontsize = 16, fontweight = 'bold')
+plt.ylabel("", fontsize = 16, fontweight = 'bold')
 # plt.ylabel("Average Connected Rate (%)", fontsize = 16, fontweight = 'bold')
 #, bbox_to_anchor = (num1, ), loc = , borderaxespad =
 
@@ -37,7 +37,7 @@ plt.rcParams.update({'font.size': 14})
 font = {'style': 'normal', 'weight': 'bold'}
 plt.legend(frameon=False, loc=9, ncol=2, bbox_to_anchor=(0.5, 1.18), prop=font)
 
-plt.savefig('TimeSlotNumbers.png', format='png')
+plt.savefig('DifferentDelta.pdf', format='pdf')
 # plt.savefig('TimeSlotConnectedRate.png', format='png')
 
 plt.show()
